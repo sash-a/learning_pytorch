bee026bf1f86299fce6319c1482181e74775434f
Sasha <reallysasha@gmail.com>
2019-02-01 12:50:43 +0200

    Removed virtual env from repo

---
+++ src/binaryor.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class bor(nn.Module):
+    def __init__(self):
+        super(bor, self).__init__()
+
+    def forward(self, *input):
+        pass
+
+    def percep(self, t):
+        pass
+
+    def train(self):
+        pass
